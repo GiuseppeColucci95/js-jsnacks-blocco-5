5c5c1121ce4352a5a9bb08d5f4b2fa6fd529b532
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 16:07:35 +0100

    add bonus snack-7

--- bonus/snack-7/script.js
+++ bonus/snack-7/script.js
@@ -22,11 +22,20 @@ const students = [
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
 
-//find with extended arrow function
-const student = students.find((student) => {
-  if (student.id === 2) {
-    return true;
-  }
-  return false;
-});
+//find with shortest arrow function version
+const student = students.find(student => student.id === 2);
 console.log(student);
+
+//classic for loop
+let studentt;
+for (let i = 0; i < students.length; i++) {
+  const thisStudent = students[i];
+  if (thisStudent.id === 2) {
+    studentt = thisStudent;
+  }
+}
+console.log(studentt);
+
+//shortest version possible
+console.log(students.find(student => student.id === 2));
+
