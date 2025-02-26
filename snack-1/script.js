5b92b03b00e75f71db128fbcd84e16619041791d
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 14:59:39 +0100

    add snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,11 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//forEach with extended arrow function
+/* names.forEach((name) => {
+  console.log(name);
+}); */
+
+//forEach with shorter arrow function
+names.forEach(name => console.log(name));
