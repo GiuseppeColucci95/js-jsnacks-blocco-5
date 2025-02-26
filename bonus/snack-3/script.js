ae1ae3955aba8bce0e5b3ae62e02333c63609d1b
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 15:40:39 +0100

    add bonus snack-3

--- bonus/snack-3/script.js
+++ bonus/snack-3/script.js
@@ -3,10 +3,20 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
-//map with extended arrow function
-const incrementedNumbers = numbers.map((numb) => {
-  return ++numb;
-});
+//map with shortest arrow function version
+const incrementedNumbers = numbers.map(numb => ++numb);
 console.log(incrementedNumbers);
+
+//classic for loop
+const incremented_numbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  let thisNumb = numbers[i];
+  incremented_numbers.push(++thisNumb);
+}
+console.log(incremented_numbers);
+
+//shortest version possible
+console.log(numbers.map(numb => ++numb));
+
 
 
