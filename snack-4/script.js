8cf371b0fb12d69690a971eb0d4b6ae820288e09
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 15:12:48 +0100

    add snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+//map with extended arrow function
+const authors = posts.map((post) => {
+  return post.author;
+});
+console.log(authors);
