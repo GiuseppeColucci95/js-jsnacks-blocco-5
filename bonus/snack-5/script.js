b8bbd075be8f3b672e2db3edde6a7cdc960240ca
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 15:50:17 +0100

    add bonus snack-5

--- bonus/snack-5/script.js
+++ bonus/snack-5/script.js
@@ -3,11 +3,22 @@ const nums = [2, 8, 4, 7, 12, 87];
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
-//filter with extended arrow function
-const evenNumbs = nums.filter((numb) => {
-  if (numb % 2 === 0) {
-    return true;
-  }
-  return false;
-});
+//filter with shortest arrow function
+const evenNumbs = nums.filter(numb => numb % 2 === 0);
 console.log(evenNumbs);
+
+//classic for loop
+const even_numbs = [];
+for (let i = 0; i < nums.length; i++) {
+  const thisNum = nums[i];
+  if (thisNum % 2 === 0) {
+    even_numbs.push(thisNum);
+  }
+}
+console.log(even_numbs);
+
+//shortest version possible
+console.log(nums.filter(numb => numb % 2 === 0));
+
+
+
