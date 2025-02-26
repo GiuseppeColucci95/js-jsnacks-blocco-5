02d4d462e96f8e0442c5f6cf992441b764055129
GiuseppeColucci95 <colucci539@gmail.com>
2025-02-26 15:28:13 +0100

    add bonus snack-1

--- bonus/snack-1/script.js
+++ bonus/snack-1/script.js
@@ -3,7 +3,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-//forEach with extended arrow function
-names.forEach((name) => {
-  console.log(name);
-});
+//forEach with shortest arrow function
+names.forEach(name => console.log(name));
+
+//classic for loop
+for (let i = 0; i < names.length; i++) {
+  const thisName = names[i];
+  console.log(thisName);
+}
+
+
